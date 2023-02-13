a360b6ee1ead608b976073a44ada75e0f7db989b
Sarah IAQUINTA <siaquinta@personnel.mines-ales.fr>
2023-02-13 09:42:48 +0100

    Introduced setup and requirement files

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="perineal_indentation",  
+    version="0.1.0",
+    author="Sarah Iaquinta",
+    author_email="sarah.r.iaquinta@gmail.com",
+    packages=find_packages(),
+    url="https://github.com/SarahIaquinta/perineal_indentation",
+    description="",  # TODO add description
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
