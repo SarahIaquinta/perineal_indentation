0f3b47fc2ef1ae66aebcee553e27f5b1721ba5c1
Sarah Iaquinta <sarah.r.iaquinta@gmail.com>
2023-04-04 13:52:20 +0200

    added a function to get the path to the Figures folder

--- indentation/experiments/laser/post_processing/utils.py
+++ indentation/experiments/laser/post_processing/utils.py
@@ -44,7 +44,7 @@ def reach_data_path(date):
     return path_to_data
 
 
-def get_path_to_processe_data():
+def get_path_to_processed_data():
     """
     Returns the path where the processed data is stored
 
@@ -62,8 +62,26 @@ def get_path_to_processe_data():
     path_to_processed_data = current_path / 'processed_data'
     return path_to_processed_data
 
+def get_path_to_figures():
+    """
+    Returns the path where the processed data is stored
+
+    Parameters:
+        ----------
+        None
+
+    Returns:
+        -------
+        path_to_processed_data: str (Path)
+            Path format string
+
+    """
+    current_path = get_current_path() 
+    path_to_processed_data = current_path / 'figures'
+    return path_to_processed_data
+
 def define_filename(filename, format):
-    path_to_processed_data = get_path_to_processe_data()
+    path_to_processed_data = get_path_to_processed_data()
     pkl_name = filename[:-4] + format
     complete_pkl_filename = path_to_processed_data / pkl_name
     return complete_pkl_filename
@@ -86,9 +104,12 @@ def extract_data_from_pkl(filename):
         
         
 def export_data_output_as_txt(filename, mat_Z, vec_time, vec_pos_axis):
-    np.savetxt(filename + '_time.txt', vec_time)
-    np.savetxt(filename + '_axis.txt', vec_pos_axis)
-    np.savetxt(filename + '_Z.txt', mat_Z)
+    filename_time = filename[:-4] + '_time.txt'
+    filename_axis = filename[:-4] + '_axis.txt'
+    filename_Z = filename[:-4] + '_Z.txt'
+    np.savetxt(get_path_to_processed_data() / filename_time, vec_time)
+    np.savetxt(get_path_to_processed_data() / filename_axis, vec_pos_axis)
+    np.savetxt(get_path_to_processed_data() / filename_Z, mat_Z)
         
 
     
