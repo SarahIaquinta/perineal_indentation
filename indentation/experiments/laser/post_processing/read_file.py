5f0233286849d13881630915d11d37b08fde4e44
Sarah Iaquinta <sarah.r.iaquinta@gmail.com>
2023-04-04 11:52:09 +0200

    Gathered code lines into functions, Exported the post processed data as a pkl and a txt file TODO document functions

--- indentation/experiments/laser/post_processing/read_file.py
+++ indentation/experiments/laser/post_processing/read_file.py
@@ -1,61 +1,149 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from math import nan
+from pathlib import Path
+import utils
+import os
 
-fime_name = 'FF1 2A.csv'
+class Files:
+    """
+    A class define the .csv files to import.
 
-cur_file = open(fime_name,'r')
-all_line = cur_file.readlines()
+    Attributes:
+        ----------
+        beginning: string
+            beginning of the csv file to be extracted
+            corresponds to the meat piece to be studied. 
+            Possible values : 
+                - FF (faux filet)
+                - RDG (rond de gite)
+            To precise the side of the meat piece (right or left),
+                add 1 or 2 after the name of the meat peace, otherwise
+                both sides will be gathered in the same list.
 
-flag_continue = False
-l_vec_Z = []
-l_time = []
-for cur_line in all_line:
-    if cur_line[0:12] == 'Frame,Source':
-        II = cur_line.find('Axis')
-        cur_line0 = cur_line[II+5:]
-        II = [pos for pos, char in enumerate(cur_line0) if char == ',']
-        vec_pos_axis = np.zeros(len(II)+1)
-        vec_pos_axis[0] = float(cur_line0[0:II[0]])
-        for it_c in range(len(II)-1):
-            vec_pos_axis[it_c+1] = float(cur_line0[II[it_c]+1:II[it_c+1]])
-        
-        vec_pos_axis[-1] = float(cur_line0[II[-1]+1:])
-        flag_continue = True
-    
-    elif flag_continue and (len(cur_line)>10):
-        II = [pos for pos, char in enumerate(cur_line) if char == ',']
-        l_time.append(float(cur_line[II[1]+1:II[2]]))
-        II = cur_line.find('Z')
-        cur_line0 = cur_line[II+1:]
-        II = [pos for pos, char in enumerate(cur_line0) if char == ',']
-        vec_Z = np.zeros(len(II)+1)
-        if II[0]==0:
-            vec_Z[0] = nan
-        else:
-            vec_Z[0] = float(cur_line0[0:II[0]])
-        
-        for it_c in range(len(II)-1):
-            if (II[it_c]+1)==(II[it_c+1]):
-                vec_Z[it_c+1] = nan
-            else:    
-                vec_Z[it_c+1] = float(cur_line0[II[it_c]+1:II[it_c+1]])
-        
-        if (II[-1]+2)==len(cur_line0):
-            vec_Z[0] = nan
-        else:
+    Methods:
+        -------
+        import_files(self):
+            Returns a list of the imported the .csv files
+
+    """
+
+    def __init__(self, beginning):
+        """
+        Constructs all the necessary attributes for the files object.
+
+        Parameters:
+            ----------
+            beginning: string
+                beginning of the csv file to be extracted
+
+        Returns:
+            -------
+            None
+        """
+
+        self.beginning = beginning
+
+    def import_files(self, date):
+        """
+        Imports the .csv files
+
+        Parameters:
+            ----------
+            date: string
+                date at which the experiments have been performed
+                Format : YYMMDD
+
+        Returns:
+            -------
+            datafile_list: list
+                list of .csv files starting by "beginning"
+
+        """
+        path_to_data_folder = utils.reach_data_path(date)
+        entries = os.listdir(path_to_data_folder)
+        beginning_to_test = date + '_' + self.beginning
+        len_beginning = len(beginning_to_test)
+        datafile_list = []
+        for i in range(len(entries)):
+            outfile = entries[i]
+            if outfile[0:len_beginning] == beginning_to_test:
+                datafile_list.append(outfile)
+        return datafile_list
+
+
+
+
+
+def read_datafile(filename):
+    date = filename[0:6]
+    path_to_data = utils.reach_data_path(date)
+    path_to_filename = path_to_data / filename
+    cur_file = open(path_to_filename,'r')
+    all_line = cur_file.readlines()
+
+    flag_continue = False
+    l_vec_Z = []
+    l_time = []
+    for cur_line in all_line:
+        if cur_line[0:12] == 'Frame,Source':
+            II = cur_line.find('Axis')
+            cur_line0 = cur_line[II+5:]
+            II = [pos for pos, char in enumerate(cur_line0) if char == ',']
+            vec_pos_axis = np.zeros(len(II)+1)
+            vec_pos_axis[0] = float(cur_line0[0:II[0]])
+            for it_c in range(len(II)-1):
+                vec_pos_axis[it_c+1] = float(cur_line0[II[it_c]+1:II[it_c+1]])
+            
             vec_pos_axis[-1] = float(cur_line0[II[-1]+1:])
+            flag_continue = True
         
-        l_vec_Z.append(vec_Z) 
+        elif flag_continue and (len(cur_line)>10):
+            II = [pos for pos, char in enumerate(cur_line) if char == ',']
+            l_time.append(float(cur_line[II[1]+1:II[2]]))
+            II = cur_line.find('Z')
+            cur_line0 = cur_line[II+1:]
+            II = [pos for pos, char in enumerate(cur_line0) if char == ',']
+            vec_Z = np.zeros(len(II)+1)
+            if II[0]==0:
+                vec_Z[0] = nan
+            else:
+                vec_Z[0] = float(cur_line0[0:II[0]])
+            
+            for it_c in range(len(II)-1):
+                if (II[it_c]+1)==(II[it_c+1]):
+                    vec_Z[it_c+1] = nan
+                else:    
+                    vec_Z[it_c+1] = float(cur_line0[II[it_c]+1:II[it_c+1]])
+            
+            if (II[-1]+2)==len(cur_line0):
+                vec_Z[0] = nan
+            else:
+                vec_pos_axis[-1] = float(cur_line0[II[-1]+1:])
+            
+            l_vec_Z.append(vec_Z) 
+    mat_Z = np.vstack(l_vec_Z)
+    vec_time = np.array(l_time)
+    
+    utils.export_data_output_as_txt(filename, mat_Z, vec_time, vec_pos_axis)
+    utils.export_data_output_as_pkl(filename, mat_Z, vec_time, vec_pos_axis)
+    return vec_time, vec_pos_axis, mat_Z
 
-mat_Z = np.vstack(l_vec_Z)
-vec_time = np.array(l_time)
+def plot_Z_profile(filename):
+    mat_Z, vec_time, vec_pos_axis = utils.extract_data_from_pkl(filename)
+    plt.imshow(mat_Z)
+    plt.show()
+    plt.close()
+    
 
-np.savetxt('time.txt',vec_time)
-np.savetxt('axis.txt',vec_pos_axis)
-np.savetxt('Z.txt',mat_Z)
-
-plt.imshow(mat_Z)
-plt.show()
-plt.close()
-
+if __name__ == "__main__":
+    current_path = utils.get_current_path()
+    experiment_date = '230403'
+    path_to_data = utils.reach_data_path(experiment_date)
+    print(path_to_data)
+    files = Files('FF')
+    list_of_FF_files = files.import_files(experiment_date)
+    filename_0 =list_of_FF_files[1]
+    read_datafile(filename_0)
+    plot_Z_profile(filename_0)
+    print('hello')
--- indentation/experiments/laser/post_processing/utils.py
+++ indentation/experiments/laser/post_processing/utils.py
@@ -2,6 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from math import nan
 from pathlib import Path
+import pickle
 
 def get_current_path(): 
     """
@@ -38,7 +39,56 @@ def reach_data_path(date):
             Path format string
 
     """
-    current_path = get_current_path()
+    current_path = get_current_path() / 'raw_data'
     path_to_data = current_path / date
     return path_to_data
+
+
+def get_path_to_processe_data():
+    """
+    Returns the path where the processed data is stored
+
+    Parameters:
+        ----------
+        None
+
+    Returns:
+        -------
+        path_to_processed_data: str (Path)
+            Path format string
+
+    """
+    current_path = get_current_path() 
+    path_to_processed_data = current_path / 'processed_data'
+    return path_to_processed_data
+
+def define_filename(filename, format):
+    path_to_processed_data = get_path_to_processe_data()
+    pkl_name = filename + format
+    complete_pkl_filename = path_to_processed_data / pkl_name
+    return complete_pkl_filename
+
+
+
+def export_data_output_as_pkl(filename, mat_Z, vec_time, vec_pos_axis):
+    complete_pkl_filename = define_filename(filename, '.pkl')
+    with open(complete_pkl_filename, "wb") as f:
+        pickle.dump(
+            [mat_Z, vec_time, vec_pos_axis],
+            f,
+        )
+        
+def extract_data_from_pkl(filename):
+    complete_pkl_filename = define_filename(filename, '.pkl')
+    with open(complete_pkl_filename, "rb") as f:
+        [mat_Z, vec_time, vec_pos_axis] = pickle.load(f)
+    return mat_Z, vec_time, vec_pos_axis
+        
+        
+def export_data_output_as_txt(filename, mat_Z, vec_time, vec_pos_axis):
+    np.savetxt(filename + '_time.txt', vec_time)
+    np.savetxt(filename + '_axis.txt', vec_pos_axis)
+    np.savetxt(filename + '_Z.txt', mat_Z)
+        
+
     
