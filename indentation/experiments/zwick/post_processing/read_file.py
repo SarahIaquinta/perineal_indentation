8f678d1d88b167989faa850b20ac848a22c7b000
Sarah Iaquinta <sarah.r.iaquinta@gmail.com>
2023-05-16 10:29:07 +0200

    fixed mispelling

--- indentation/experiments/zwick/post_processing/read_file.py
+++ indentation/experiments/zwick/post_processing/read_file.py
@@ -6,11 +6,11 @@ import utils
 import os
 from indentation.experiments.laser.figures.utils import CreateFigure, Fonts, SaveFigure
 from tqdm import tqdm
-
+import pandas as pd
 
 class Files_Zwick:
     """
-    A class define the .csv files to import.
+    A class define the .xls files to import.
 
     Attributes:
         ----------
@@ -23,7 +23,7 @@ class Files_Zwick:
     Methods:
         -------
         import_files(self):
-            Returns a list of the imported the .csv files
+            Returns a list of the imported the .xls files
 
     """
 
@@ -48,7 +48,7 @@ class Files_Zwick:
 
     def import_files(self, date):
         """
-        Imports the .csv files
+        Imports the .xls files
 
         Parameters:
             ----------
@@ -59,7 +59,7 @@ class Files_Zwick:
         Returns:
             -------
             datafile_list: list
-                list of .csv files starting by "type_of_essay"
+                list of .xls files starting by "type_of_essay"
 
         """
         path_to_data_folder = utils.reach_data_path(date)
@@ -73,6 +73,29 @@ class Files_Zwick:
                 datafile_list.append(outfile)
         return datafile_list
 
+    def get_sheets_from_datafile(self, datafile):
+        """
+        Lists the sheets in the xls file that contain data
+
+        Parameters:
+            ----------
+            datafile: string
+                name of the datafile to be read
+
+        Returns:
+            -------
+            sheets_list_with_data: list
+                list of the sheets in the datafile
+
+        """
+        date = datafile[0:6]
+        path_to_datafile = utils.reach_data_path(date)
+        file = pd.ExcelFile(path_to_datafile/datafile)
+        sheets_list = file.sheet_names
+        sheets_list_with_data = [i for i in sheets_list if i.startswith(date)]
+        return sheets_list_with_data
+        
+
 if __name__ == "__main__":
     # createfigure = CreateFigure()
     # fonts = Fonts()
@@ -81,4 +104,6 @@ if __name__ == "__main__":
     types_of_essay = ['C_Indentation_relaxation_500N_force']#, 'RDG']
     files_zwick = Files_Zwick(types_of_essay[0])
     datafile_list = files_zwick.import_files(experiment_dates[0])
+    datafile = datafile_list[0]
+    sheets_in_datafile = files_zwick.get_sheets_from_datafile(datafile)
     print('hello')
