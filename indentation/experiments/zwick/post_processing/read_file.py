6d9a512b7b73b95c7161c25ca48f017947841f10
Sarah Iaquinta <sarah.r.iaquinta@gmail.com>
2023-05-30 14:40:08 +0200

    Created a function to identify failed testings, ie those where the force values are too small or too large

--- indentation/experiments/zwick/post_processing/read_file.py
+++ indentation/experiments/zwick/post_processing/read_file.py
@@ -105,7 +105,7 @@ class Files_Zwick:
         path_to_datafile = utils.reach_data_path(date)
         datafile_as_pds = pd.ExcelFile(path_to_datafile/datafile)
         sheets_list = datafile_as_pds.sheet_names
-        sheets_list_with_data = [i for i in sheets_list if i.startswith(date)]
+        sheets_list_with_data = [i for i in sheets_list if i.startswith(date) or i.startswith('RDG') or i.startswith('FF')]
         return datafile_as_pds, sheets_list_with_data
         
         
@@ -133,6 +133,7 @@ class Files_Zwick:
         date = datafile[0:6]
         path_to_datafile = utils.reach_data_path(date) / datafile
         data_in_sheet = pd.read_excel(path_to_datafile, sheet_name=sheet, header=2, names=["s", "N", "mm" ], usecols="A:C", decimal=',') 
+        print(sheet)
         time = data_in_sheet.s
         force = data_in_sheet.N
         disp = data_in_sheet.mm
@@ -149,6 +150,22 @@ class Files_Zwick:
         speed_dict = {ids.tolist()[i]: speed.tolist()[i] for i in range(len(ids.tolist()))}
         return imposed_disp_dict, speed_dict
         
+    def is_testing_failed(self, datafile, sheet):
+        _, force, _ = self.read_sheet_in_datafile(datafile, sheet)
+        bool = False
+        max_force = np.max(force)
+        if max_force > 1.2 or max_force < 1:
+            bool = True
+        return bool
+    
+    def find_only_correct_sheets_in_datafile(self, datafile):
+        _, sheets_list_with_data = self.get_sheets_from_datafile(datafile)
+        list_of_correct_sheets = [sheet for sheet in sheets_list_with_data if not self.is_testing_failed(datafile, sheet)]
+        return list_of_correct_sheets
+
+            
+            
+            
         
     def plot_data_from_sheet(self, datafile, sheet, createfigure, savefigure, fonts):
         time, force, disp = self.read_sheet_in_datafile(datafile, sheet)
@@ -180,7 +197,36 @@ class Files_Zwick:
         savefigure.save_as_png(fig_disp_vs_time, sheet + "_disp_vs_time")
         savefigure.save_as_png(fig_force_vs_disp, sheet + "_force_vs_disp")
         
+        
+    def plot_data_from_sheet_meat(self, datafile, sheet, createfigure, savefigure, fonts):
+        time, force, disp = self.read_sheet_in_datafile(datafile, sheet)
+        fig_force_vs_time = createfigure.rectangle_rz_figure(pixels=180)
+        fig_disp_vs_time = createfigure.rectangle_rz_figure(pixels=180)
+        fig_force_vs_disp = createfigure.rectangle_rz_figure(pixels=180)
+        ax_force_vs_time = fig_force_vs_time.gca()
+        ax_disp_vs_time = fig_disp_vs_time.gca()
+        ax_force_vs_disp = fig_force_vs_disp.gca()
+        date = datafile[0:6]
+        kwargs = {"color":'k', "linewidth": 2}
+        ax_force_vs_time.plot(time, force, **kwargs)
+        ax_disp_vs_time.plot(time, disp, **kwargs)
+        ax_force_vs_disp.plot(disp, force, **kwargs)
+        ax_force_vs_time.set_xlabel(r"time [s]", font=fonts.serif(), fontsize=26)
+        ax_disp_vs_time.set_xlabel(r"time [s]", font=fonts.serif(), fontsize=26)
+        ax_force_vs_disp.set_xlabel(r"U [mm]", font=fonts.serif(), fontsize=26)
+        ax_force_vs_time.set_ylabel(r"Force [N]", font=fonts.serif(), fontsize=26)
+        ax_disp_vs_time.set_ylabel(r"U [mm]", font=fonts.serif(), fontsize=26)
+        ax_force_vs_disp.set_ylabel(r"Force [N]", font=fonts.serif(), fontsize=26)
+        # ax_force_vs_time.legend(prop=fonts.serif(), loc='center right', framealpha=0.7)
+        # ax_disp_vs_time.legend(prop=fonts.serif(), loc='center right', framealpha=0.7)
+        # ax_force_vs_disp.legend(prop=fonts.serif(), loc='center right', framealpha=0.7)
+        savefigure.save_as_png(fig_force_vs_time, date + '_' + sheet + "_force_vs_time")
+        savefigure.save_as_png(fig_disp_vs_time, date + '_' + sheet + "_disp_vs_time")
+        savefigure.save_as_png(fig_force_vs_disp, date + '_' + sheet + "_force_vs_disp")
+        
 
+
+ 
 
 
 
@@ -189,16 +235,17 @@ if __name__ == "__main__":
     createfigure = CreateFigure()
     fonts = Fonts()
     savefigure = SaveFigure()
-    experiment_dates = ['230515']#, '230411']#'230331', '230327', '230403']
-    types_of_essay = ['C_Indentation_relaxation_maintienFnulle_500N_trav.xls']#, 'RDG']
+    experiment_dates = ['230407', '230331', '230411', '230327', '230403']
+    types_of_essay = ['C_Indentation_relaxation_500N_force.xlsx']#,'C_Indentation_relaxation_maintienFnulle_500N_trav.xls',  'RDG']
     files_zwick = Files_Zwick(types_of_essay[0])
     datafile_list = files_zwick.import_files(experiment_dates[0])
     datafile = datafile_list[0]
     datafile_as_pds, sheets_list_with_data = files_zwick.get_sheets_from_datafile(datafile)
-    sheet1 = sheets_list_with_data[0]
-    # time, force, disp = files_zwick.read_sheet_in_datafile(datafile, sheet1)
-    for sheet in sheets_list_with_data:
-        files_zwick.plot_data_from_sheet( datafile, sheet, createfigure, savefigure, fonts)
+    correct_sheets_in_data = files_zwick.find_only_correct_sheets_in_datafile(datafile)
+    sheet1 = correct_sheets_in_data[0]
+    time, force, disp = files_zwick.read_sheet_in_datafile(datafile, sheet1)
+    for sheet in correct_sheets_in_data:
+        files_zwick.plot_data_from_sheet_meat( datafile, sheet, createfigure, savefigure, fonts)
     # metadatafile = files_zwick.import_metadatafile(experiment_dates[0])
     # imposed_disp_dict, speed_dict = files_zwick.read_metadatas_zwick(metadatafile)
     print('hello')
