b07d5dfce2884292ca8d354c1d97e7af3445d5f5
Sarah Iaquinta <sarah.r.iaquinta@gmail.com>
2023-06-01 18:14:17 +0200

    commented code

--- indentation/model/comsol/sensitivity_analysis/generate_dataset.py
+++ indentation/model/comsol/sensitivity_analysis/generate_dataset.py
@@ -1,5 +1,3 @@
-import itertools
-from math import pi
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import qmc
@@ -7,24 +5,52 @@ import matplotlib.font_manager as font_manager
 import seaborn as sns
 
 def generate_dataset(expected_sample_size):
+    """
+    Generates a Sobol quasi Monte Carlo dataset
+
+    Parameters:
+        ----------
+        expected_sample_size: float
+            expected size for the dataset
+            the real size of the generated dataset is the closest inferior power of 2
+
+    Returns:
+        -------
+        lambda_list, damage_list: lists
+            lists of values of lambda and damage
+
+    """
     sampler = qmc.Sobol(d=2, scramble=False)
     n = int(np.log(expected_sample_size) / np.log(2))
     sample_size = int(2 ** n)
     print("Sample size = ", sample_size)
-    sample = sampler.random_base2(m=n)  # defining sample size = 4096
+    sample = sampler.random_base2(m=n)  
     var1 = np.array([sample[i][0] for i in range(len(sample))])
     var2 = np.array([sample[i][1] for i in range(len(sample))])
     lambda_max = 1
     lambda_min = 1.5
     damage_max = 0
     damage_min = 0.9
-
     lambda_list = [i * (lambda_max - lambda_min) + lambda_min for i in var1]
     damage_list = [i * (damage_max - damage_min) + damage_min for i in var2]
-
     return lambda_list, damage_list
 
 def export_dataset_as_txt(lambda_list, damage_list):
+    """
+    Exports a dataset as a txt file
+
+    Parameters:
+        ----------
+        lambda_list: list
+            list of values of lambda
+        damage_list: list
+            list of values of damage
+            
+    Returns:
+        -------
+        None
+
+    """
     path_to_dataset = r'C:\Users\siaquinta\Documents\Projet Périnée\perineal_indentation\indentation\model\comsol\sensitivity_analysis'
     complete_txt_filename = path_to_dataset + "/comsol_input_dataset_size" + str(len(lambda_list)) +".txt"
     f = open(complete_txt_filename, "w")
@@ -42,6 +68,21 @@ def export_dataset_as_txt(lambda_list, damage_list):
     f.close()
 
 def plot_dataset(lambda_list, damage_list):
+    """
+    Plots the dataset 
+
+    Parameters:
+        ----------
+        lambda_list: list
+            list of values of lambda
+        damage_list: list
+            list of values of damage
+
+    Returns:
+        -------
+        None
+
+    """
     path_to_figure = r'C:\Users\siaquinta\Documents\Projet Périnée\perineal_indentation\indentation\model\comsol\sensitivity_analysis'
     complete_figure_filename = path_to_figure + "/comsol_input_dataset_size" + str(len(lambda_list)) +".png"
     fig = plt.figure(figsize=(7, 7), dpi=180, constrained_layout=True)
@@ -57,7 +98,6 @@ def plot_dataset(lambda_list, damage_list):
     ax.set_yticks([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9], font = font_manager.FontProperties(family="serif", weight="normal", style="normal", size=18))
     ax.set_yticklabels([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9], font = font_manager.FontProperties(family="serif", weight="normal", style="normal", size=18))
     fig.savefig(complete_figure_filename, format="png")
-
 
 
 if __name__ == "__main__":
