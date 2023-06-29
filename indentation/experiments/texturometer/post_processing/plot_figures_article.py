ed2b1712a47ec9f75697266f4d803ee004506576
Sarah Iaquinta <sarah.r.iaquinta@gmail.com>
2023-06-29 07:05:13 +0200

    solved saving issues

--- indentation/experiments/texturometer/post_processing/plot_figures_article.py
+++ indentation/experiments/texturometer/post_processing/plot_figures_article.py
@@ -1,13 +1,20 @@
-""""
-This file is used to generate nice looking figures for illustrating articles.
-"""
-
 import numpy as np
 from matplotlib import pyplot as plt
+from math import nan
+from pathlib import Path
+import utils
+import os
 from indentation.experiments.texturometer.figures.utils import CreateFigure, Fonts, SaveFigure
+import indentation.experiments.laser.post_processing.read_file as rf
+import indentation.experiments.laser.post_processing.display_profiles as dp
+from indentation.experiments.laser.post_processing.read_file import Files
 import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from scipy.signal import lfilter
 import pickle
+import csv
 import pandas as pd
+import statistics
 import seaborn as sns
 import pandas as pd
 from indentation.experiments.zwick.post_processing.utils import find_nearest
@@ -27,6 +34,8 @@ def get_data_from_file():
     index_where_disp_is_disp_at_20 = np.where(disp == find_nearest(disp, disp_at_20))[0][0]
     force_at_20 = force.to_list()[index_where_disp_is_disp_at_20]
     return disp, force, disp_at_20, force_at_20, disp_at_80, force_at_80
+
+
 
 def plot_force_vs_disp(createfigure, savefigure, fonts):
     disp, force, disp_at_20, force_at_20, disp_at_80, force_at_80 = get_data_from_file()
@@ -51,9 +60,13 @@ def plot_force_vs_disp(createfigure, savefigure, fonts):
     ax_force_vs_disp.annotate(r"$F_{20\%}$", ((disp.to_list()[0]+ disp_at_20)/2 -0.5 , force_at_20 + 2), color=color_F20, font=fonts.serif(), fontsize=22)
     ax_force_vs_disp.annotate(r"$F_{80\%}$", ((disp.to_list()[0]+ disp_at_80)/2 -0.5 , force_at_80 + 2), color=color_F80, font=fonts.serif(), fontsize=22)
 # ax_A_vs_force80_1.annotate(maturation_dict_plots[date], (mean_force80_FF1_dict[date] +0.04, mean_A_FF1_dict[date]+0.02), color = color[7], bbox=dict(boxstyle="round", fc="0.8", alpha=0.4))
+
     savefigure.save_as_png(fig_force_vs_disp, 'texturometer_article')
 
+
+
 def plot_forces_with_maturation():
+    maturation = [10, 13, 17, 21]
     path_to_processed_data = r'C:\Users\siaquinta\Documents\Projet Périnée\perineal_indentation\indentation\experiments\texturometer\processed_data'
     complete_pkl_filename = path_to_processed_data + "/forces_mean_std.pkl"
     with open(complete_pkl_filename, "rb") as f:
@@ -64,16 +77,25 @@ def plot_forces_with_maturation():
              mean_force20_FF_dict, std_force20_FF_dict, mean_force80_FF_dict, std_force80_FF_dict,
              mean_force20_RDG_dict, std_force20_RDG_dict, mean_force80_RDG_dict, std_force80_RDG_dict
              ] = pickle.load(f)
+        
+    
     dates_to_use = ['230331', '230403', '230407']
     maturation_dict = {'230331': 10, '230403': 13, '230407': 17}
     color = sns.color_palette("Paired")
     color_rocket = sns.color_palette("rocket")
+    kwargs_FF1 = {'marker':'o', 'mfc':color[6], 'elinewidth':3, 'ecolor':color[6], 'alpha':0.8, 'ms':'20', 'mec':color[6]}
     kwargs_FF = {'marker':'o', 'mfc':color_rocket[3], 'elinewidth':3, 'ecolor':color_rocket[3], 'alpha':0.8, 'ms':'20', 'mec':color_rocket[3]}
+    kwargs_FF2 = {'marker':'o', 'mfc':color[7], 'elinewidth':3, 'ecolor':color[7], 'alpha':0.8, 'ms':'20', 'mec':color[7]}
+    kwargs_RDG1 = {'marker':'^', 'mfc':color[0], 'elinewidth':3, 'ecolor':color[0], 'alpha':0.8, 'ms':10, 'mec':color[0]}
+    kwargs_RDG2 = {'marker':'^', 'mfc':color[1], 'elinewidth':3, 'ecolor':color[1], 'alpha':0.8, 'ms':'20', 'mec':color[1]}
     kwargs_RDG = {'marker':'^', 'mfc':color_rocket[1], 'elinewidth':3, 'ecolor':color_rocket[1], 'alpha':0.8, 'ms':'20', 'mec':color_rocket[1]}
     maturation_FF_dict = {k: v - 0.1 for k, v in maturation_dict.items()}
     maturation_RDG_dict = {k: v + 0.1 for k, v in maturation_dict.items()}
+
+
     fig_force20 = createfigure.rectangle_figure(pixels=180)
     ax_force20 = fig_force20.gca()
+
     [dates, mean_force20_FF1_dict, std_force20_FF1_dict, mean_force80_FF1_dict, std_force80_FF1_dict,
              mean_force20_FF2_dict, std_force20_FF2_dict, mean_force80_FF2_dict, std_force80_FF2_dict,
              mean_force20_RDG1_dict, std_force20_RDG1_dict, mean_force80_RDG1_dict, std_force80_RDG1_dict,
@@ -96,8 +118,11 @@ def plot_forces_with_maturation():
     ax_force20.set_xticklabels([10, 13, 17], font=fonts.serif(), fontsize=24)
     ax_force20.set_yticks([0, 5, 10, 15])
     ax_force20.set_yticklabels([0, 5, 10, 15], font=fonts.serif(), fontsize=24)
+
     savefigure.save_as_png(fig_force20, "article_force20_vs_maturation_1+2")
     plt.close(fig_force20)
+
+
     fig_force80 = createfigure.rectangle_figure(pixels=180)
     ax_force80 = fig_force80.gca()
     ax_force80.errorbar(list(maturation_FF_dict.values()), list(mean_force80_FF_dict.values()), yerr=list(std_force80_FF_dict.values()), lw=0, label='FF', **kwargs_FF)
@@ -111,6 +136,10 @@ def plot_forces_with_maturation():
     ax_force80.set_yticklabels([0, 25, 50, 75, 100], font=fonts.serif(), fontsize=24)
     savefigure.save_as_png(fig_force80, "article_force80_vs_maturation_1+2")
     plt.close(fig_force80)
+
+
+
+
 
 if __name__ == "__main__":
     createfigure = CreateFigure()
