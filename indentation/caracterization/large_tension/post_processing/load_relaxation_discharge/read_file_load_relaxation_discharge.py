75aada873f0ec8b4a257bd3d5dff0a2c6dc8a8f7
Sarah Iaquinta <sarah.r.iaquinta@gmail.com>
2023-11-08 12:29:17 +0100

    moved plot functions for raw data into plot_raw_data.py file

--- indentation/caracterization/large_tension/post_processing/load_relaxation_discharge/read_file_load_relaxation_discharge.py
+++ indentation/caracterization/large_tension/post_processing/load_relaxation_discharge/read_file_load_relaxation_discharge.py
@@ -340,27 +340,6 @@ def plot_experimental_data(datafile, sheet):
     print(sheet, 'done')
     
 
-def plot_experimental_data_3D(datafile, sheet):
-    time, elongation, stress = read_sheet_in_datafile(datafile, sheet)
-    # times_at_elongation_steps, stress_at_elongation_steps, elongation_steps = find_end_load_peaks(datafile, sheet)
-    beginning_load_phase_indices_list, end_load_phase_indices_list, beginning_relaxation_phase_indices_list, end_relaxation_phase_indices_list, beginning_discharge_phase_indices_list, end_discharge_phase_indices_list = find_peaks_handmade(datafile, sheet)
-    load_phase_time_dict, relaxation_phase_time_dict, discharge_phase_time_dict, load_phase_stress_dict, relaxation_phase_stress_dict, discharge_phase_stress_dict, load_phase_elongation_dict, relaxation_phase_elongation_dict, discharge_phase_elongation_dict = gather_data_per_steps(datafile, sheet)
-    number_of_steps = len(load_phase_time_dict)
-    fig = createfigure.rectangle_figure(pixels=180)
-    date = datafile[0:6]
-
-    ax = fig.gca()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(time, elongation, stress, c = sns.color_palette("flare", len(elongation)),
-                    lw=0, antialiased=False, s = 1 + np.zeros_like(np.array(stress)), alpha = 1)
-    ax.set_xlabel(r"time [s]", font=fonts.serif(), fontsize=26)
-    ax.set_ylabel(r"$\lambda_x$ [-]", font=fonts.serif(), fontsize=26)
-    ax.set_zlabel(r"$\Pi_x^{exp}$ [kPa]", font=fonts.serif(), fontsize=26)
-    plt.close(fig)
-    savefigure.save_as_png(fig, date + "_" + sheet + "_3D_exp")
-
-
-
     
 if __name__ == "__main__":
     createfigure = CreateFigure()
@@ -376,7 +355,6 @@ if __name__ == "__main__":
     # time, elongation, stress = read_sheet_in_datafile(datafile, sheet1)
     # plot_experimental_data(datafile, sheet1)
     for sheet in sheets_list_with_data:
-        # plot_experimental_data_3D(datafile, sheet)
         plot_experimental_data(datafile, sheet)
         # export_data_per_steps(datafile, sheet)
     # find_peaks(datafile, sheet1)
